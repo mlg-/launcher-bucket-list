fbdd22e86001d256adae7a7a43d722fde59b4cff
Melissa Leigh Gore <melissaleighgore@gmail.com>
2016-03-02 14:33:29 -0500

    Finish changing example

--- spec/features/01_user_sees_grocery_list_spec.rb
+++ spec/features/01_user_sees_grocery_list_spec.rb
@@ -2,21 +2,21 @@ require "spec_helper"
 
 feature "user sees a list of favorite things" do
   scenario "user views favorites" do
-    CSV.open("favorites_list.csv", "a", headers: true) do |file|
-      thing = "Ruby"
+    CSV.open("bucket_list.csv", "a", headers: true) do |file|
+      thing = "Get my first dev job!"
       file.puts([thing])
     end
 
-    visit "/favorites-list"
-    expect(page).to have_content("Ruby")
+    visit "/bucket-list"
+    expect(page).to have_content("Get my first dev job!")
   end
 
-  scenario "user sees all favorites at root path" do
-    CSV.open("favorites_list.csv", "a", headers: true) do |file|
-      thing = "Pysduck"
+  scenario "user sees all bucket list goals at root path" do
+    CSV.open("bucket_list.csv", "a", headers: true) do |file|
+      thing = "Get published in Rattle magazine"
       file.puts([thing])
     end
     visit "/"
-    expect(page).to have_content("Psyduck")
+    expect(page).to have_content("Get published in Rattle magazine")
   end
 end
